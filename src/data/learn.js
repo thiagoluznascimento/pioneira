db1638f54a44e0a87199de5e266d088b64e99d56
Thiago <thiagoifnmgsi2015@gmail.com>
2024-09-25 16:49:16 -0300

    hotfix: Adiciona pagina de fique por dentro e modifica pagina produtos.

---
+++ src/data/learn.js
@@ -0,0 +1,63 @@
+import product1 from "../assets/learn/analise-de-solo.jpg";
+import product2 from "../assets/learn/rotacao-de-culturas-destaque.webp";
+import product3 from "../assets/learn/irrigacao.jpg";
+import product4 from "../assets/learn/cobertura-do-solo.jpg";
+import product5 from "../assets/learn/controle-praga.png";
+import product6 from "../assets/learn/tecnologia-campo.webp";
+import product7 from "../assets/learn/adubacao-verde-agroecologia.jpg";
+import product8 from "../assets/learn/certificacao.png";
+import product9 from "../assets/learn/compostagem.webp";
+import product10 from "../assets/learn/planejamento-de-safra.jpg";
+
+export const learnList = [
+    {
+        title: "Análise de solo",
+        description: "Realize análises regulares do solo para obter informações precisas sobre a fertilidade e os nutrientes presentes. Isso ajudará a determinar quais ajustes e correções são necessários, permitindo a aplicação adequada de fertilizantes e corretivos para melhorar a qualidade do solo.",
+        image: product1
+    },
+    {
+        title: "Rotação de culturas",
+        description: "Pratique a rotação de culturas para evitar o esgotamento excessivo de nutrientes específicos no solo. Cultivar diferentes culturas em diferentes épocas do ano ajuda a equilibrar a demanda de nutrientes e reduz a incidência de doenças e pragas específicas de uma única cultura.",
+        image: product2
+    },
+    {
+        title: "Irrigação eficiente",
+        description: "Implemente sistemas de irrigação eficientes para maximizar o uso da água. A irrigação por gotejamento e a captação de água da chuva são ótimas práticas que podem reduzir o desperdício e garantir que suas plantas recebam a umidade necessária para o crescimento saudável.",
+        image: product3
+    },
+    {
+        title: "Uso de cobertura vegetal",
+        description: "Utilize coberturas vegetais para proteger o solo e melhorar a estrutura do solo. As coberturas ajudam a conservar a umidade, prevenir a erosão e aumentar a matéria orgânica, resultando em um solo mais saudável e produtivo.",
+        image: product4
+    },
+    {
+        title: "Controle de pragas",
+        description: "Adote práticas de controle integrado de pragas (CIP) que combinam métodos biológicos, químicos e culturais. Isso não apenas protege suas culturas, mas também promove um ambiente de cultivo sustentável e saudável.",
+        image: product5
+    },
+    {
+        title: "Tecnologia no campo",
+        description: "Invista em tecnologia, como sensores e drones, para monitorar a saúde das culturas e otimizar a gestão da fazenda. Essas ferramentas podem fornecer dados em tempo real e insights valiosos para decisões mais informadas.",
+        image: product6
+    },
+    {
+        title: "Adubação verde",
+        description: "Considere o uso de adubação verde, que consiste no cultivo de plantas específicas para melhorar a fertilidade do solo. Essas plantas não apenas adicionam nutrientes ao solo quando incorporadas, mas também ajudam a prevenir a erosão e a compactação.",
+        image: product7
+    },
+    {
+        title: "Certificações de qualidade",
+        description: "Busque certificações que atestem a qualidade e a sustentabilidade de seus produtos. Isso não apenas agrega valor ao seu negócio, mas também atende à crescente demanda por práticas agrícolas responsáveis e sustentáveis.",
+        image: product8
+    },
+    {
+        title: "Compostagem",
+        description: "Pratique a compostagem para transformar resíduos orgânicos em adubo natural. Essa técnica não só reduz o desperdício, mas também enriquece o solo com nutrientes essenciais, promovendo uma agricultura mais sustentável.",
+        image: product9
+    },
+    {
+        title: "Planejamento de safra",
+        description: "Desenvolva um planejamento de safra que leve em consideração as condições climáticas, o mercado e a capacidade de produção. Um bom planejamento pode otimizar os recursos e maximizar os lucros ao longo do ano.",
+        image: product10
+    }
+];
--- src/screens/About/index.jsx
+++ src/screens/About/index.jsx
@@ -6,15 +6,8 @@ import '../styles.css'
 function About() {
     return(
         <Container>
-            <h2 className="titulo-h2">Nossa História</h2>
-            <p>Fundada em [ano de fundação], a Terragro nasceu do desejo de transformar o agronegócio com produtos de alta qualidade e tecnologias inovadoras. Iniciamos nossas operações focados em fornecer sementes de excelência, desenvolvidas por meio de pesquisas avançadas e cuidadosamente selecionadas para atender às necessidades dos agricultores brasileiros. Desde o início, nossa missão sempre foi clara: ajudar os produtores rurais a aumentar sua produtividade de forma sustentável e eficiente.</p>
-            
-            <p>Com o tempo, expandimos nosso portfólio para incluir máquinas e equipamentos agrícolas, como drones pulverizadores e cortadores de grama de alta performance. Nossos produtos foram criados para simplificar o trabalho no campo, oferecendo mais praticidade e resultados superiores para produtores de todos os portes. Ao longo dessa jornada, consolidamos parcerias estratégicas com centros de pesquisa e especialistas agronômicos, garantindo que nossas soluções estivessem sempre à frente das inovações do setor.</p>
-            
-            <p>Além de tecnologia, temos um compromisso sólido com a sustentabilidade. Desde o início, implementamos práticas que respeitam o meio ambiente, desenvolvendo sementes e tecnologias que promovem o uso responsável dos recursos naturais. Essa visão nos permitiu crescer e nos tornar uma referência no setor agrícola, atendendo clientes em diversas regiões do Brasil.</p>
-            
-            <p>Hoje, a Terragro continua a crescer, sempre guiada pelos valores que nos trouxeram até aqui: inovação, sustentabilidade e excelência. Nossa equipe trabalha diariamente para oferecer soluções que impulsionam o futuro do agronegócio, ajudando agricultores a alcançar o sucesso e a produzir com responsabilidade.</p>
-
+            <h2 className="titulo-h2">Quem somos</h2>
+            <h3 className="sub-titulo-h3 " >VISÃO, MISSÃO E VALOR</h3>
             <Accordion defaultActiveKey="0">
                 <Accordion.Item eventKey="0">
                     <Accordion.Header>Visão</Accordion.Header>
@@ -39,14 +32,25 @@ function About() {
                     <strong>Qualidade:</strong> Garantimos a excelência em cada produto e serviço oferecido, sempre buscando superar as expectativas de nossos clientes. <br />
                     <strong>Parceria:</strong> Valorizamos relações de confiança com nossos clientes, fornecedores e colaboradores, trabalhando lado a lado para alcançar os melhores resultados. <br />
                     <strong>Ética:</strong> Nossa conduta é pautada pela transparência e respeito em todas as nossas relações, garantindo um ambiente de negócios íntegro e justo. <br />
-                    <strong>Compromisso com o Cliente:</strong> Nossa prioridade é oferecer soluções que agreguem valor ao negócio dos nossos clientes, ajudando-os a alcançar o sucesso em suas atividades agrícolas.
+                    <strong>Compromisso com o Cliente:</strong> Nossa prioridade é oferecer soluções que agreguem valor ao negócio dos nossos clientes, ajudando-os a alcançar o sucesso em suas atividades agrícolas. <br />
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
-            <h3 className="sub-titulo-h3 " >Sustentabilidade</h3>
-            <p>A sustentabilidade sempre foi uma das nossas principais preocupações. Desde o início, buscamos desenvolver soluções que minimizem o impacto ambiental e promovam o uso responsável dos recursos naturais.</p>
-            <p>Acreditamos que o futuro da agricultura depende de práticas sustentáveis, e trabalhamos constantemente para inovar nesse sentido. Um dos nossos marcos importantes foi o lançamento de sementes adaptadas a diferentes condições climáticas e resistentes a pragas, ajudando os agricultores a produzir mais com menos recursos, mantendo o foco na preservação do solo e da biodiversidade.</p>
 
+            <h3 className="sub-titulo-h3">Nossa História</h3>
+            <p>Fundada em [ano de fundação], a Terragro nasceu do desejo de transformar o agronegócio com produtos de alta qualidade e tecnologias inovadoras. Iniciamos nossas operações focados em fornecer sementes de excelência, desenvolvidas por meio de pesquisas avançadas e cuidadosamente selecionadas para atender às necessidades dos agricultores brasileiros. Desde o início, nossa missão sempre foi clara: ajudar os produtores rurais a aumentar sua produtividade de forma sustentável e eficiente.</p>
+            <p>Com o tempo, expandimos nosso portfólio para incluir máquinas e equipamentos agrícolas, como drones pulverizadores e cortadores de grama de alta performance. Nossos produtos foram criados para simplificar o trabalho no campo, oferecendo mais praticidade e resultados superiores para produtores de todos os portes. Ao longo dessa jornada, consolidamos parcerias estratégicas com centros de pesquisa e especialistas agronômicos, garantindo que nossas soluções estivessem sempre à frente das inovações do setor.</p>
+            <p>Além de tecnologia, temos um compromisso sólido com a sustentabilidade. Desde o início, implementamos práticas que respeitam o meio ambiente, desenvolvendo sementes e tecnologias que promovem o uso responsável dos recursos naturais. Essa visão nos permitiu crescer e nos tornar uma referência no setor agrícola, atendendo clientes em diversas regiões do Brasil.</p>            
+            <p>Hoje, a Terragro continua a crescer, sempre guiada pelos valores que nos trouxeram até aqui: inovação, sustentabilidade e excelência. Nossa equipe trabalha diariamente para oferecer soluções que impulsionam o futuro do agronegócio, ajudando agricultores a alcançar o sucesso e a produzir com responsabilidade.</p>
+
+            <h3 className="sub-titulo-h3" >Sustentabilidade</h3>
+            <p>A sustentabilidade sempre foi uma das nossas principais preocupações. Desde o início, buscamos desenvolver soluções que minimizem o impacto ambiental e promovam o uso responsável dos recursos naturais. Acreditamos que o futuro da agricultura depende de práticas sustentáveis, e trabalhamos constantemente para inovar nesse sentido. Um dos nossos marcos importantes foi o lançamento de sementes adaptadas a diferentes condições climáticas e resistentes a pragas, ajudando os agricultores a produzir mais com menos recursos, mantendo o foco na preservação do solo e da biodiversidade.</p>
+
+            <h3 className="sub-titulo-h3" >Inovação Contínua</h3>
+            <p>A Terragro acredita que a inovação é a chave para o futuro do agronegócio. Estamos constantemente investindo em pesquisas e novas tecnologias para oferecer soluções mais eficientes e modernas aos nossos clientes. Nossos produtos são desenvolvidos com foco na tecnologia de ponta, garantindo que os agricultores tenham acesso ao que há de melhor para melhorar a produtividade e enfrentar os desafios do campo. Trabalhamos lado a lado com centros de pesquisa e especialistas para que a inovação esteja presente em cada uma de nossas soluções, do plantio à colheita.</p>
+
+            <h3 className="sub-titulo-h3">Parceria com o Produtor Rural</h3>
+            <p>Valorizamos profundamente a parceria com o agricultor e entendemos que cada fazenda, cada colheita, tem suas próprias particularidades. Por isso, oferecemos um atendimento personalizado, com foco em entender as necessidades específicas de cada cliente e oferecer soluções que realmente façam a diferença. Estamos ao lado dos produtores em todas as etapas do processo, desde o planejamento até a execução, oferecendo suporte técnico, consultoria especializada e produtos que agregam valor. A confiança que construímos ao longo dos anos com nossos clientes é a base do nosso sucesso.</p> <br />
         </Container>
     );
 }
