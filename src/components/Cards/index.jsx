1f7497d3348cb83561220bd81c291a36b9b00b04
Thiago <thiagoifnmgsi2015@gmail.com>
2024-09-27 16:04:10 -0300

    Adiciona modal no componente card de produtos.

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,8 +1,18 @@
-import { Card, Row, Col, Container, Button } from "react-bootstrap";
+import { Card, Row, Col, Container, Button, Modal } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 import { products } from "../../data/products";
+import { useState } from "react";
 
-function Cards() {
+function Cards(props) {
+
+    const [texto, setTexto] = useState([]);  //useState é o Hook -> ele retorna um par de valores
+    const [modalShow, setModalShow] = useState(false);
+    const [selectedProduct, setSelectedProduct] = useState(null)
+
+    const handleShow = (product) => {
+        setSelectedProduct(product); // Para armazenar o produto selecionado
+        setModalShow(true); //Para mostrar modal
+    }
 
     return(
         <Container className="p-3">
@@ -13,27 +23,58 @@ function Cards() {
                         <Card.Img variant="top" src={product.image} className="img-fluid" style={{ objectFit: "cover", width: "100%", height: "200px" }} />
                         <Card.Body>
                             <Card.Title>{product.title}</Card.Title>
-                                <Card.Text>{product.description}</Card.Text>
+                                <Card.Text>
+                                    {texto[idx]
+                                        ? product.description
+                                        : `${product.description.substring(0, 50)}...`
+                                    }
+                                </Card.Text>
                             <Button size="sm" variant="success" className="ms-3">
                                 Comprar
                                 <Link to="/" className="text-white ms-1">
                                     <i className="bi bi-whatsapp"></i>
                                 </Link>
                             </Button>
-                            <Button size="sm" variant="success" className="ms-3">
+                            <Button size="sm" variant="success" className="ms-3" onClick={() => handleShow(product)}>
                                 Ver mais
-                                <Link to="/" className="text-white ms-1">
+
+                                <Link to="" className="text-white ms-1">
                                     <i className="bi bi-eye"></i>
-                                </Link>                                
+                                </Link>
                             </Button>
                         </Card.Body>
                     </Card>
                 </Col>
             ))}
             </Row>
+
+            {/* lembrar que sempre o modal tem que ser fora do loop */}
+            <Modal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        {selectedProduct ? selectedProduct.title : ''} {/* Título do produto selecionado */}
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {selectedProduct &&(
+                    <>
+                    <img src={selectedProduct.image} alt={selectedProduct.title} className="img-fluid" style={{ objectFit: "cover", width: "100%", height: "400px" }} />
+                    <p>{selectedProduct ? selectedProduct.description : ''} {/* Descrição do produto selecionado */}</p>
+                    </>
+                    )}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="success" onClick={() => setModalShow(false)}>Fechar</Button>
+                </Modal.Footer>
+            </Modal>
         </Container>
     )
 }
 
 export default Cards;
-
